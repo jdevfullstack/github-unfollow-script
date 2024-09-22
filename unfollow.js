2c3deca42b5a6d9e848ec804290e32602ca5f88c
jdevfullstack <jtechdev001@gmail.com>
2024-09-22 18:09:25 +0800

    Update unfollow.js

--- unfollow.js
+++ unfollow.js
@@ -1,26 +1,33 @@
-const { Octokit } = require("@octokit/rest");
+(async () => {
+  const { Octokit } = await import("@octokit/rest");
 
-// create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-const octokit = new Octokit({ auth: `your_token_with_correct_permission` });
+  // create a personal access token at https://github.com/settings/tokens/new?scopes=user:follow
+  const octokit = new Octokit({ auth: `your_token_with_correct_permission` });
 
-async function unfollowNonFollowers() {
-  const username = 'your_username';
+  async function unfollowNonFollowers() {
+    const username = 'your_username';
 
-  // Get list of users who are following you
-  const followers = await octokit.paginate(octokit.rest.users.listFollowersForUser, { username });
-  const followerLogins = followers.map(user => user.login);
+    // Get list of users who are following you
+    const followers = await octokit.paginate(octokit.rest.users.listFollowersForUser, { username });
+    const followerLogins = followers.map(user => user.login);
 
-  // Get list of users you are following
-  const following = await octokit.paginate(octokit.rest.users.listFollowingForUser, { username });
-  const followingLogins = following.map(user => user.login);
+    // Get list of users you are following
+    const following = await octokit.paginate(octokit.rest.users.listFollowingForUser, { username });
+    const followingLogins = following.map(user => user.login);
 
-  // Unfollow users who are not following you
-  for (const user of followingLogins) {
-    if (!followerLogins.includes(user)) {
-      await octokit.rest.users.unfollow({ username: user });
-      console.log(`Unfollowed ${user}`);
+    let unfollowedCount = 0;
+
+    // Unfollow users who are not following you
+    for (const user of followingLogins) {
+      if (!followerLogins.includes(user)) {
+        await octokit.rest.users.unfollow({ username: user });
+        unfollowedCount++;
+      }
     }
-  }
-}
 
-unfollowNonFollowers().catch(console.error);
+    // Log the total number of unfollowed users
+    console.log(`unfollowed ${unfollowedCount} users`);
+  }
+
+  unfollowNonFollowers().catch(console.error);
+})();
